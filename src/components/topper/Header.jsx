7ae20d7f738765d2d36c3f13cebc9a6b7dd38dca
Brian <brian.rico378@gmail.com>
2023-08-25 06:03:54 +0200

    Header parte 1

--- src/components/topper/Header.jsx
+++ src/components/topper/Header.jsx
@@ -1,4 +1,5 @@
-//import logo from '../../assets/img/logo-UNAN.png'
+import { NavBar } from './NavBar'
+import logo from '../../assets/img/logo-UNAN.png'
 
 export const Header = () => {
 
@@ -6,11 +7,19 @@ export const Header = () => {
 
   return (
     <>
-      <header>
-          <div className=" bg-[#AB030C] ">
-            <p className="text-white ml-10 lg:ml-20 pt-1 pb-1 font-poppins ">UNAN-León</p>
-          </div>
-          {/* <img src={logo} alt="Logo-Unanleon" /> */}
+      <div className=" bg-[#AB030C] ">
+        <p className="text-white ml-10 lg:ml-20 pt-1 pb-1 font-poppins ">Universidad Autónoma de Nicaragua </p>
+      </div>
+      <header className='flex place-content-between'>
+        <div className='flex items-center'>
+          <img src={logo} alt="Logo-Unanleon" className='w-32 h-33 m-4 ' />
+          <p className='font-roboto'>
+            <span className='text-[#202C75] text-2xl font-[700] '>UNAN </span> <span className='text-[#000] text-2xl font-[700] '> - </span>
+            <span className='text-[#910618] text-2xl font-[700] '>LEON </span> <span className='text-[#605656] text-3xl font-[800] '> | </span>
+            <span className='text-[#605656] text-lg font-[400] '>REPOSITORIO</span>
+          </p>
+        </div>
+        <NavBar /> 
       </header>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
       },
       fontFamily: {
         'poppins': [ 'Poppins' ],
-        
+        'roboto': [ 'Roboto' ],
       },
     },
   },
