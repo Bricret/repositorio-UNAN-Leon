b6e9d0d26997268099f0c74cb2ceb86ef5213987
Brian <brian.rico378@gmail.com>
2023-08-25 23:16:54 +0200

    Creacion del hero y formulario

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,12 +1,20 @@
-import imgHero from "../../assets/img/hero.jpg";
+import { UseForm } from "../../utils/hooks/useForm";
 
 export const Hero = () => {
     
+    const { Search, setSearch } = UseForm({
+        search: '',
+    });
+
   return (
     <>
-        <div>
-            <img src={ imgHero } alt="hero_img" />
-        </div>
+        <div className=" sm:h-screen h-96 grid text-center bg-cover bg-center bg-[url('https://upload.wikimedia.org/wikipedia/commons/4/45/UNAN_Le%C3%B3n.jpg')]">
+            <div className="col-start-1 row-start-1 bg-gray-950 bg-opacity-80 w-full h-full"></div>
+            <div className="col-start-1 row-start-1 mx-auto my-auto">
+                <p className=" text-white font-poppins sm:text-6xl font-bold">¿Qué estas buscando?</p>
+
+            </div>
+        </div> 
     </>
     )
 }
--- src/components/topper/BurgerMenu.jsx
+++ src/components/topper/BurgerMenu.jsx
@@ -3,7 +3,7 @@ export const BurgerMenu = () => {
     <>
         <ul className="hidden sm:flex sm:w-[438px] sm:mr-4 sm:place-content-around sm:items-center sm:font-[400] sm:text-[#605656]">
             <li className=''>
-                <a href="#" className='w-[100%] px-3 py-1  hover:text-orange-400'>Home</a>
+                <a href="#" className='w-[100%] px-3 py-1  hover:text-orange-400'>Pagina Principal</a>
             </li>
             <li>
             <a href="#" className='w-[100%] px-3 py-1 hover:text-orange-400'>Aulas virtuales</a>    
@@ -12,11 +12,12 @@ export const BurgerMenu = () => {
                 <a href="#" className='w-[100%] px-3 py-1 hover:text-orange-400'>Contáctanos</a>
             </li>
         </ul>
+        
         <div className='mr-6  mt-[24px]  sm:hidden cursor-pointer' >   
-            <span className='block w-3/4 h-[6px] m-1 bg-black  rounded' ></span>
-            <span className='block w-3/4 h-[6px] m-1 bg-black mt-1 rounded' ></span>
-            <span className='block w-3/4 h-[6px] m-1 bg-black mt-1 mb-2 rounded ' ></span>
-            <span className=' font-[poppins] underline hover:underline-offset-4'>Menu</span>
+            <span className='block w-3/4 h-[5px] m-1 bg-black  rounded' ></span>
+            <span className='block w-3/4 h-[5px] m-1 bg-black mt-1 rounded' ></span>
+            <span className='block w-3/4 h-[5px] m-1 bg-black mt-1 rounded ' ></span>
+            <span className='font-[poppins] text-xs underline hover:underline-offset-4'>Menu</span>
         </div>
     </>
   )
