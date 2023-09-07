a3bdc93ddc1e8dd20676e2ba53a73633be009ed4
Brian <brian.rico378@gmail.com>
2023-09-07 03:53:31 +0200

    aplicacion de icono al search

--- src/components/hero/Search.jsx
+++ src/components/hero/Search.jsx
@@ -1,23 +1,23 @@
-// import lupa from '../../assets/icons/lupa.svg';
+import { AiOutlineSearch } from "react-icons/ai";
 
 
 export const Search = () => {
   return (
     <>
         <form action="#" className="flex flex-col md:flex-row justify-start items-center gap-4 font-poppins font-semibold px-8 relative" >
+
             <input 
                 type= "text"
-                className= " sm:w-full w-full md:w-10/12 h-8 md:h-14 rounded-md sm:text-lg text-xs border-2 border-rose-500 px-10 focus:outline-none"
+                className= "sm:w-full w-full md:w-10/12 h-8 md:h-14 rounded-md sm:text-lg text-xs border-none ring-2 ring-red-300 focus:ring-red-500 focus:ring-2 px-10 focus:outline-none"
                 placeholder= "Escribe el nombre de examenes de grado o tesis"
                 //onSubmit={}
             />
-            {/* <div className=" absolute inset-y-0 left-0 flex items-center">
-              <img src={lupa} alt="lupa icono" className='h-4 w-4 mx-3'/>
-            </div> */}
-            <button href='#' className=" bg-primary-color  rounded-md text-slate-50 text-xs md:text-base w-32 md:w-2/12 h-8 md:h-14">Buscar</button>
+            <div className="absolute sm:inset-y-0 mt-1 sm:mt-0  left-10 flex items-center text-2xl text-slate-600">
+                <AiOutlineSearch />
+            </div>
+
+            <button href='#' className=" bg-primary-color rounded-md text-slate-50 text-xs md:text-base w-32 md:w-2/12 h-8 md:h-14">Buscar</button>
         </form>
     </>
   )
 }
-
-//integrar icono de busqueda al form
