47ce2a6f6272605cf360e33bdbc9ee93e0912613
Brian <brian.rico378@gmail.com>
2023-09-10 12:52:57 -0600

    mejora de footer

--- src/components/footer/ItemsFooter.jsx
+++ src/components/footer/ItemsFooter.jsx
@@ -6,13 +6,19 @@ export const ItemsFooter = () => {
         <>
             <div className="flex flex-wrap md:justify-center justify-start mt-10 pb-32 pt-16 md:flex-row flex-col gap-24 px-10 md:px-0 text-third-color">
                 {
-                    seccion.map(( { title, seccion1, seccion2, seccion3 } , index ) => (
-                        <section key={ index } className=" text-start">
+                    seccion.map(( { title, seccion1, seccion2, seccion3, style, link1, link2, link3 } , index ) => (
+                        <section key={ index } className=" sm:text-start text-center">
                             <h2 className=" pb-6 font-bold text-2xl cursor-default">{ title }</h2>
                             <ul className="text-white">
-                                <li className="mb-5 cursor-pointer" >{ seccion1 }</li>
-                                <li className="mb-5 cursor-pointer" >{ seccion2 }</li>
-                                <li className="mb-5 cursor-pointer" >{ seccion3 }</li>
+                                <li className={ style } >
+                                    <a href={ link1 }> { seccion1 } </a>
+                                </li>
+                                <li className={ style } >
+                                    <a href={ link2 }>{ seccion2 }</a>
+                                </li>
+                                <li className={ style } >
+                                    <a href={ link3 }>{ seccion3 }</a>
+                                </li>
                             </ul>  
                         </section> 
                     ))
--- src/components/footer/seccion.js
+++ src/components/footer/seccion.js
@@ -1,4 +1,4 @@
-
+const style = "mb-5 cursor-pointer";
 
 const seccion = [
     {
@@ -6,30 +6,42 @@ const seccion = [
         title: 'Ubicanos',
         seccion1: 'Edificio Central',
         seccion2: 'Campus Medico',
-        seccion3: 'Campus residencial'
+        seccion3: 'Campus residencial',
+        style,
     },
 
     {
         id: 2,
         title: 'Universidad',
-        seccion1: 'estatuas UNAN-Leon',
+        seccion1: 'estatutos UNAN-Leon',
+        link1: 'https://drive.google.com/file/d/0Bzn1TxJq1gDFRmx5ZllLcXUtYlk/view?resourcekey=0-kVhdMSO5otH6Gntfn3ysJQ',
         seccion2: 'Reseña Histórica de la UNAN-León',
+        link2: 'https://unanleon.edu.ni/UNANLEON_Historia.html',
         seccion3: 'Pensamiento Estratégico', 
+        link3: 'https://unanleon.edu.ni/UNANLEON_PensamientoEstrategico.html',
+        style,
     },
 
     {
         id: 3,
         title: 'Recursos',
         seccion1: 'Sistema de Bibliotecas - SIBUL',
+        link1: 'https://sibul.unanleon.edu.ni/',
         seccion2: 'Portal de Revistas: Científica y Literarias',
+        link2: 'https://revistas.unanleon.edu.ni/',
+        style
     },
 
     {
         id: 4,
         title: 'Servicios',
         seccion1: 'Red Wifi',
+        link1: 'https://unanleon.edu.ni/UNANLEON_DI.html',
         seccion2: 'Guía Telefónica',
+        link2: 'https://unanleon.edu.ni/UNANLEON_guiatelef.html',
         seccion3: 'Correo Office 365',
+        link3: 'https://unanleon.edu.ni/UNANLEON_guiatelef.html',
+        style,
     },
 ]
 
